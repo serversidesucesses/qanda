4dafb904154fc301a904d6e07d47a97570e98898
Isabelle <izzigrace6@gmail.com>
2022-08-05 16:42:42 -0700

    make changes to route

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const pool = new Pool({
   user: process.env.DB_USERNAME,
   host: process.env.DB_HOSTNAME,
   database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 3000
 })
 
